656a8b0aaed7c4530960583642b04c394a3fe7f8
Conrad Ku <c_mku@outlook.com>
2019-02-23 22:38:42 -0600

    added tips - Conrad Ku

--- HealthAdviceJava/src/HealthAdvice.java
+++ HealthAdviceJava/src/HealthAdvice.java
@@ -13,8 +13,20 @@ public class HealthAdvice {
 			"Be sure to get at least 8 hours of sleep for an energized morning!",
 			"Eat vegetables! They contain plenty of vitamins, minerals, and antioxidants!",
 			"Avoid smoking to reduce risk of lung cancer.",
-			"Reduce intake of red meats to prevent cholesterol buildup."
-
+			"Reduce intake of red meats to prevent cholesterol buildup.",
+			"Stay hydrated!" ,
+			"Studies have shown that cutting down on sugary foods can greatly improve energy, mood, as well help with weight loss!" , 
+			"Vaccines are essential for a long healthy life!",
+			"Don't underestimate the effect of staying active! Just fifteen minutes a day can improve mood.",
+			"A doctor a day can keep the apple away!",
+			"An apple a day keeps the doctor away!" , 
+			"Smoking is bad for one's health!",
+			"Mental health is important! Reach out to others if you need help!",
+			"Get tested!",
+			"Keep your head up!"
+			"Protection is key!",
+			"Don't trust everything you read online! (Including me!)"
+ 
 		};
 		return advice[rand.nextInt(advice.length)];
 	}
